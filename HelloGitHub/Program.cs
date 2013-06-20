6fc16b28c24138ce26851c5d3a1b96bd7cda03f5
Robert Harris <dominus794@gmail.com>
2013-06-20 17:21:38 +0100

    Redundant WriteLine Removed
    Removed the redundant WriteLine call for this simple Hello GitHub test.

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -10,7 +10,6 @@ namespace HelloGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("This was created in a local branch, merged with the master, and then the testbranch deleted.");
         }
     }
 }
